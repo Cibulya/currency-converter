b45f9c6ade563a924a72a8417a450d3fde3c492b
Aliaksandr Konik <cibulyadev@gmail.com>
2023-06-17 14:40:31 +0200

    refactor:refactor styles

--- curr-app/src/pages/404.tsx
+++ curr-app/src/pages/404.tsx
@@ -1,4 +1,6 @@
+import styles from '@/styles/notfound.module.scss';
 import Head from 'next/head';
+import Link from 'next/link';
 
 const NotFoundPage = () => {
   return (
@@ -6,7 +8,12 @@ const NotFoundPage = () => {
       <Head>
         <title>Currency-app: 404</title>
       </Head>
-      <div>This page not exists</div>
+      <div className={styles.content}>
+        <h1>Такой страницы не существует.</h1>
+        <Link className={styles.link} href="/">
+          Вернуться на главную
+        </Link>
+      </div>
     </>
   );
 };
